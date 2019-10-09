82e6b4120a11ad3005013449cdd7cc3ea54e9114
Winston James Kruger <WinstonK@awards.co.za>
2019-10-09 17:21:03 +0200

    Update skeleton

--- Maintenance.php
+++ Maintenance.php
@@ -19,26 +19,26 @@ class Maintenance {
             $data = json_decode($request->body);
         }catch(Exception $e){
             return $e;
-        }
-        var_dump($data);
-        exit;
+        }   
     }
 
     public function read($id) {
-
         try{
-
             $url = "http://localhost:8080/HospitalityResort/maintenance/read/{$id}";
             $request = Requests::get($url, array('Content-type' => 'application/json'));
-            $data = json_decode($request->body);
-            
+            $data = json_decode($request->body);           
+        }catch(Exception $e){
+            return $e;
+        }      
+    }
+
+    public function update($id){
+        try{
+
         }catch(Exception $e){
             return $e;
         }
-        // var_dump($data);
-        // exit;
     }
-
 }
 
 ?>
