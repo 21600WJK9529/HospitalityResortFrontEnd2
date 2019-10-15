2ef48d31b961a39f390b9bb6c1518cbe9c5e7f56
Winston James Kruger <WinstonK@awards.co.za>
2019-10-15 15:39:11 +0200

    Added nav bar and restyled pages

--- Footer.php
+++ Footer.php
@@ -6,14 +6,5 @@
         <link rel="stylesheet" href="assets/css/style.css">
         <link rel="icon" href="http://example.com/favicon.png"><!--Icon for tab in browser-->
     </head>
-<?php
-include '/Header.php'
-?>
-    <body style="background-image:url('original.jpg'); background-repeat: no-repeat; background-size: cover">
-        <div class="container" >
-            <h1 style="text-align: center;margin-top:40%;margin-left: 21%;background-color: #212529a1; opacity:0.9; color:white;max-width: 66%;"> Please select option from navigation bar</h1>
-            
-        </div>
-    </body>
 
-</html>
+</html>    
